1a05284b1e87302dbe0e7a8cd39927312e4dd20b
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2020-09-20 20:49:46 -0300

    feat: success and error messages
    a prettier way to return success and error messages

--- src/app/validators/student.js
+++ src/app/validators/student.js
@@ -1,5 +1,34 @@
-const BaseValidator = require('./base')
+const Teacher = require('../models/Teacher')
+
+function isEmpty(fields) {
+    const keys = Object.keys(fields)
+    
+    for(key of keys)
+        if(!fields[key])
+            return true
+    
+    return false
+}
+
+async function post(req, res, next) {
+    if(isEmpty(req.body)) {
+        const teachers = await Teacher.all()
+        return res.render('student/create', { error: 'Por favor, preencha todos os campos!', student: req.body, teachers })
+    }
+
+    return next()
+}
+
+async function update(req, res, next) {
+    if(isEmpty(req.body)) {
+        const teachers = await Teacher.all()
+        return res.render('student/edit', { error: 'Por favor, preencha todos os campos!', student: req.body, teachers })
+    }
+
+    return next()
+}
 
 module.exports = {
-    ...BaseValidator
+    post,
+    update
 }
--- src/app/validators/teacher.js
+++ src/app/validators/teacher.js
@@ -1,5 +1,28 @@
-const BaseValidator = require('./base')
+function isEmpty(fields) {
+    const keys = Object.keys(fields)
+    
+    for(key of keys)
+        if(!fields[key])
+            return true
+    
+    return false
+}
+
+function post(req, res, next) {
+    if(isEmpty(req.body))
+        return res.render('teacher/create', { error: 'Por favor, preencha todos os campos!', teacher: req.body })
+
+    return next()
+}
+
+function update(req, res, next) {
+    if(isEmpty(req.body))
+        return res.render('teacher/edit', { error: 'Por favor, preencha todos os campos!', teacher: req.body })
+
+    return next()
+}
 
 module.exports = {
-    ...BaseValidator
+    post,
+    update
 }
